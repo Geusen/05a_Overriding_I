4cabb1beb706c3cd43811e398560ed1182ee0520
David <paulussen86@gmail.com>
2024-01-30 11:57:27 +0100

    Grandparent ++

--- com/cc/java/Parent.java
+++ com/cc/java/Parent.java
@@ -1,11 +1,11 @@
 package com.cc.java;
 
 public class Parent extends Grandparent {
-    public String sayHello(){
-        return "Hello from parent!";
-    }
+    // public String sayHello(){
+    //     return "Hello from parent!";
+    // }
 
-    public String sayHello(String str){
-        return str;
-    }
+public String sayHello(String str){
+     return str;
+}
 }
